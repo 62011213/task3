090bc76003e24fe31470a0a488eafaef7649ecde
62011213 Pitchakorn Pichetnaruemit <62011213@kmitl.ac.th>
2021-02-18 18:14:51 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from Tranasportation import Tranasportation
+from Walk import Walk
+
+
+
+if __name__ == '__main__':
+    travel_cost = 0
+
+    trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+            Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+            Train("Ladkrabang Station","Payathai Station",40,6),
+            Taxi("Payathai Station","The British Council",3) ]
+
+    for travel in trip:
+    travel_cost += travel.find_cost()
+    print(travel_cost)
