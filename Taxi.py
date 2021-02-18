abc6bf0df529b67a1922eb10a4f6a8c12bbb3f05
62011213 Pitchakorn Pichetnaruemit <62011213@kmitl.ac.th>
2021-02-18 18:51:11 +0700

    Update Taxi.py

--- Taxi.py
+++ Taxi.py
@@ -5,6 +5,5 @@ class Taxi(Transportation):
     def __init__(self, start, end, distance):
         Transportation.__init__(self, start, end, distance)
 
-
     def find_cost( self ):
-        return self.distance * 50
+        return self.distance * 40
